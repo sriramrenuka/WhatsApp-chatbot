909a26e871591b818411e33e2315c4924526becb
sriramrenuka <127374969+sriramrenuka@users.noreply.github.com>
2024-02-09 20:59:10 +0530

    Add files via upload

---
+++ appp.py
@@ -0,0 +1,16 @@
+import pyautogui
+import webbrowser as wb
+import time
+wb.open("web.whatsapp.com")
+time.sleep(30)
+for i in range(10):
+    pyautogui.press("p")
+    pyautogui.press("i")
+    pyautogui.press("enter")
+    pyautogui.press("g")
+    pyautogui.press("r")
+    pyautogui.press("i")
+    pyautogui.press("l")
+    pyautogui.press("enter")
+     
+     
